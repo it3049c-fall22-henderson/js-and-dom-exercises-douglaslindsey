c5ac075dfc9bddaa2e2b1bfedb3e374a03351c7f
Douglas Lindsey <lindsedj@mail.uc.edu>
2022-09-21 23:42:57 -0400

    updated word counter

--- exercises/word-counter/js/app.js
+++ exercises/word-counter/js/app.js
@@ -10,6 +10,24 @@ const log = document.getElementById('values');
 
 input.addEventListener('input', updateValue);
 
-function updateValue(e) {
-  log.textContent = e.target.value;
+function wordCount(val) {
+  var wom = val.match(/\S+/g);
+  return {
+    charactersNoSpaces: val.replace(/\s+/g, '').length,
+    characters: val.length,
+    words: wom ? wom.length : 0,
+    lines: val.split(/\r*\n/).length
+  };
 }
+var textarea = document.getElementById('text');
+var result = document.getElementById('result');
+
+textarea.addEventListener('input', function() {
+  var wc = wordCount(this.value);
+  result.innerHTML = (`
+   <br>Characters (no spaces):  ${wc.charactersNoSpaces}
+   <br>Characters (and spaces): ${wc.characters}
+   <br>Words: ${wc.words}
+   <br>Lines: ${wc.lines}
+  `);
+});
